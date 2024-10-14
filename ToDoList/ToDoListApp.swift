274bf237113363cc96e26caaaa4f38876568c7ee
Akshay kumar shaw <akshay.8459361606@gmail.com>
2024-10-14 10:19:35 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Akshay kumar shaw on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
