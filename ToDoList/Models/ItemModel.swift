8466b4980ae77065e08922c7dda976f2f3ff7621
Akshay kumar shaw <akshay.8459361606@gmail.com>
2024-10-14 20:34:31 +0530

    App Completed

---
+++ ToDoList/Models/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  ToDoList
+//
+//  Created by Akshay kumar shaw on 14/10/24.
+//
+
+import Foundation
---
+++ ToDoList/View/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  ToDoList
+//
+//  Created by Akshay kumar shaw on 14/10/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
